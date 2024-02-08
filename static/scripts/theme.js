7ccb77fbb377779f05e5b08750951a02329adf7b
Aesara Binder <aesara@mailbox.org>
2024-02-08 22:53:50 +1030

    Themes: More consistent toggle (sys/dark/light over boolean)

--- static/scripts/theme.js
+++ static/scripts/theme.js
@@ -1,16 +1,48 @@
-// Declarations
 const rootElem = document.documentElement;
-let themeSwitch = (localStorage.getItem("themeSwitch") === "true")
+let currentTheme = localStorage.getItem("currentTheme")
 
-// Colour theme
-if (themeSwitch) {
-	rootElem.classList.toggle("themeSwitch");
+if (!currentTheme) {
+	currentTheme = "systemTheme"
+	localStorage.setItem("currentTheme", currentTheme);
 }
 
-function colourThemer(button) {
-	rootElem.classList.toggle("themeSwitch");
-	if (!button) {
-		themeSwitch = !themeSwitch;
-		localStorage.setItem("themeSwitch", themeSwitch.toString());
+function rotateThemes() {
+	switch (currentTheme) {
+		case "systemTheme":
+			currentTheme = "darkTheme"
+			break;
+		case "darkTheme":
+			currentTheme = "lightTheme"
+			break;
+		case "lightTheme":
+			currentTheme = "systemTheme"
+			break;
+	}
+	localStorage.setItem("currentTheme", currentTheme);
+}
+function setTheme() {
+	switch (currentTheme) {
+		case "lightTheme":
+			rootElem.classList.add("lightTheme");
+			rootElem.classList.remove("darkTheme");
+			rootElem.classList.remove("systemTheme");
+			break;
+		case "darkTheme":
+			rootElem.classList.remove("lightTheme");
+			rootElem.classList.add("darkTheme");
+			rootElem.classList.remove("systemTheme");
+			break;
+		default:
+			rootElem.classList.remove("lightTheme");
+			rootElem.classList.remove("darkTheme");
+			rootElem.classList.add("systemTheme");
+			break;
 	}
 }
+
+function themer() {
+	rotateThemes();
+	setTheme();
+}
+
+setTheme();
